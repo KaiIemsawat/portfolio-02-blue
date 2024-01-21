4b5221066a9c9f5c4f5def2757e6370236d7e597
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-01-21 01:38:40 -0500

    initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,45 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        colors: {
+            // Blue
+            "cblue-20": "#f6f2ff",
+            "cblue-50": "#e6e2fa",
+            "cblue-100": "#d4d1eb",
+            "cblue-200": "#a9a3d7",
+            "cblue-300": "#7d75c2",
+            "cblue-400": "#5247ae",
+            "cblue-500": "#27199a",
+            "cblue-600": "#1f147b",
+            "cblue-700": "#170f5c",
+            "cblue-800": "#100a3e",
+            "cblue-900": "#08051f",
+
+            // Pink
+            "cpink-20": "#fff3fb",
+            "cpink-50": "#ffe8f2",
+            "cpink-100": "#fed6e6",
+            "cpink-200": "#fdadcd",
+            "cpink-300": "#fc83b4",
+            "cpink-400": "#fb5a9b",
+            "cpink-500": "#fa3182",
+            "cpink-600": "#c82768",
+            "cpink-700": "#961d4e",
+            "cpink-800": "#641434",
+            "cpink-900": "#320a1a",
+
+            "cgreen-100": "#d4f9d6",
+            "cgreen-200": "#a8f4ad",
+            "cgreen-300": "#7dee83",
+            "cgreen-400": "#51e95a",
+            "cgreen-500": "#26e331",
+            "cgreen-600": "#1eb627",
+            "cgreen-700": "#17881d",
+            "cgreen-800": "#0f5b14",
+            "cgreen-900": "#082d0a",
+        },
+        extend: {},
+    },
+    plugins: [],
+};
